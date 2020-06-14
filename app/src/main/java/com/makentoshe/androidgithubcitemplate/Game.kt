c324d606afb858729d8f2133e737ddb95834e48b
Alexander-Sergeenkov <sergeenkov1alexander@gmail.com>
2020-06-14 12:23:35 +0300

    Синхронизация с игровым окном

---
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/Game.kt
@@ -0,0 +1,11 @@
+package com.makentoshe.androidgithubcitemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
