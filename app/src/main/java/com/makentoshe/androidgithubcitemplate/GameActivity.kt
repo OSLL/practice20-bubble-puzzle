dded74400d90130bdfcb3cd1935c9980e27d6718
Andrew <tankist.scratch.p@gmail.com>
2020-06-15 15:28:46 +0300

    Linking activity with game timer

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/GameActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/GameActivity.kt
@@ -1,11 +1,29 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.makentoshe.androidgithubcitemplate.gameLogic.Game
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    private fun onTick(timeLost: Int) {
+        val minutes: Int = timeLost / 60
+        val seconds = timeLost % 60
+
+        this.textTimeLost.text = "$minutes:$seconds"
+    }
+
+    private fun onFinish() {
+        val goToRestart = Intent(this, GameActivity::class.java)
+        startActivity(goToRestart)
+    }
+
+    private val gameApi = Game({ i -> onTick(i) }, { onFinish() })
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
